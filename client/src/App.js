e2f460b55e4fe1727ccb5ea40335f20b4b7565a1
kevin-salamon <kevinwebdevs@gmail.com>
2020-07-21 12:46:38 -0400

    completed most redux implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Posts from "./components/Posts";
 import Postsform from "./components/Postsform";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from "redux";
+
+const store = createStore(() => [], {}, applyMiddleware());
 
 function App() {
   return (
-    <>
-    <Postsform />
-    <Posts />
-    </>
+    <Provider store={store}>
+      <>
+      <Postsform />
+      <Posts />
+      </>
+    </Provider>
   )
 }
 
