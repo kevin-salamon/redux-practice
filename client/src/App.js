25e48ef02b82b80962fedaed7f265c37df3e81e7
kevin-salamon <kevinwebdevs@gmail.com>
2020-07-22 11:17:30 -0400

    implemented redux dev tools chrome extension

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,8 @@ import React from 'react';
 import Posts from "./components/Posts";
 import Postsform from "./components/Postsform";
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from "redux";
 
-const store = createStore(() => [], {}, applyMiddleware());
+import store from './store';
 
 function App() {
   return (
