f9be9cb3170779a442861fdb786efcdafeb36d3e
kevin-salamon <kevinwebdevs@gmail.com>
2020-07-22 12:04:46 -0400

    completed addition of AddPost function through redux

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS, NEW_POSTS } from './types';
+import { FETCH_POSTS, NEW_POST } from './types';
 
 export const fetchPosts = () => dispatch => {
     console.log("fetching...");
@@ -11,3 +11,19 @@ export const fetchPosts = () => dispatch => {
             })
         );
 }
+
+export const createPost = (postData) => dispatch => {
+    console.log("adding...");
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(postData)
+    })
+    .then(res => res.json())
+    .then(post => dispatch({
+        type: NEW_POST,
+        payload: post
+    }));
+};
